65a0d7571d06770bb9572856ba9584567713007c
1cihanemre <90394648+1cihanemre@users.noreply.github.com>
2022-07-05 15:30:36 +0300

    Update and rename distance_sensor_with_servo.ino to distance_sensor_with_servo_and_buzzer.ino

--- distance_sensor_with_servo_and_buzzer.ino
+++ distance_sensor_with_servo_and_buzzer.ino
@@ -10,23 +10,27 @@ void setup() {
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin,INPUT);
   Serial.begin(9600);
+  pinMode(2,OUTPUT);
 
 }
 
 void loop() {
-  myservo.write(0);
-  delay(500);
-  digitalWrite(trigPin,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin,LOW);
-  sure=pulseIn(echoPin,HIGH);
-  uzaklik=sure/2/29.154;
-
+    myservo.write(0);
+    delay(500);
+    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin,LOW);
+    sure=pulseIn(echoPin,HIGH);
+    uzaklik=sure/2/29.154;
   if(uzaklik<10){
-    Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-    delay(1000);
+    digitalWrite(2,HIGH);
+    delay(200);
+    digitalWrite(2,LOW);
+    Serial.print(uzaklik);
+    Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+    delay(100);
     myservo.write(30);
     delay(500);
     digitalWrite(trigPin,LOW);
@@ -37,8 +41,12 @@ void loop() {
     sure=pulseIn(echoPin,HIGH);
     uzaklik=sure/2/29.154;
     if(uzaklik<10){
-      Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-      delay(1000);
+      digitalWrite(2,HIGH);
+      delay(200);
+      digitalWrite(2,LOW);
+      Serial.print(uzaklik);
+      Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+      delay(100);
       myservo.write(60);
       delay(500);
       digitalWrite(trigPin,LOW);
@@ -49,8 +57,12 @@ void loop() {
       sure=pulseIn(echoPin,HIGH);
       uzaklik=sure/2/29.154;
           if(uzaklik<10){
-            Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-            delay(1000);
+            digitalWrite(2,HIGH);
+            delay(200);
+            digitalWrite(2,LOW);
+            Serial.print(uzaklik);
+            Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+            delay(100);
             myservo.write(90);
             delay(500);
             digitalWrite(trigPin,LOW);
@@ -61,8 +73,12 @@ void loop() {
             sure=pulseIn(echoPin,HIGH);
             uzaklik=sure/2/29.154;
               if(uzaklik<10){
-                Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-                delay(1000);
+                digitalWrite(2,HIGH);
+                delay(200);
+                digitalWrite(2,LOW);
+                Serial.print(uzaklik);
+                Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+                delay(100);
                 myservo.write(120);
                 delay(500);
                 digitalWrite(trigPin,LOW);
@@ -73,8 +89,12 @@ void loop() {
                 sure=pulseIn(echoPin,HIGH);
                 uzaklik=sure/2/29.154;
                 if(uzaklik<10){
-                  Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-                  delay(1000);
+                  digitalWrite(2,HIGH);
+                  delay(200);
+                  digitalWrite(2,LOW);
+                  Serial.print(uzaklik);
+                  Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+                  delay(100);
                   myservo.write(150);
                   delay(500);
                   digitalWrite(trigPin,LOW);
@@ -85,8 +105,12 @@ void loop() {
                   sure=pulseIn(echoPin,HIGH);
                   uzaklik=sure/2/29.154;
                   if(uzaklik<10){
-                    Serial.println("Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
-                    delay(1000);
+                    digitalWrite(2,HIGH);
+                    delay(200);
+                    digitalWrite(2,LOW);
+                    Serial.print(uzaklik);
+                    Serial.println("cm Yakinda Nesne algilandi!\nRotasyon Degistiriliyor!");
+                    delay(100);
                     myservo.write(180);
                     delay(500);
                     digitalWrite(trigPin,LOW);
@@ -99,32 +123,50 @@ void loop() {
                   }
                   else{
                     Serial.println("10cm yakinda nesne algilanamadi!");
+                    Serial.print("En yakin nesne ");
+                    Serial.print(uzaklik);
+                    Serial.println("cm uzaklikta");
                     delay(1000);
                   }
                 }
                 else{
                   Serial.println("10cm yakinda nesne algilanamadi!");
+                  Serial.print("En yakin nesne ");
+                  Serial.print(uzaklik);
+                  Serial.println("cm uzaklikta");
                   delay(1000);
                 }
               }
               else{
                 Serial.println("10cm yakinda nesne algilanamadi!");
+                Serial.print("En yakin nesne ");
+                Serial.print(uzaklik);
+                Serial.println("cm uzaklikta");
                 delay(1000);
               }
           }
           else{
             Serial.println("10cm yakinda nesne algilanamadi!");
+            Serial.print("En yakin nesne ");
+            Serial.print(uzaklik);
+            Serial.println("cm uzaklikta");
             delay(1000);
           }
     }
     else{
       Serial.println("10cm yakinda nesne algilanamadi!");
+      Serial.print("En yakin nesne ");
+      Serial.print(uzaklik);
+      Serial.println("cm uzaklikta");
       delay(1000);
     }
     
   }
   else{
     Serial.println("10cm yakinda nesne algilanamadi!");
+    Serial.print("En yakin nesne ");
+    Serial.print(uzaklik);
+    Serial.println("cm uzaklikta");
     delay(1000);
   }
 }
